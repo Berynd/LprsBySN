8a4d4cb97e198317610e0223177114023cfe4ee9
Eythanne <e.passard@lprs.frgit config --global user.email e.passard@lprs.fr>
2025-09-23 12:11:48 +0200

    feat modification utilisateur repository

---
+++ src/traitement/TraitementUtilsateur.php
@@ -0,0 +1,36 @@
+<?php
+
+include "../repository/UtilisateurRepository.php";
+require_once "../bdd/BDD.php";
+require_once "../modele/Utilisateur.php";
+var_dump($_POST);
+
+if(empty($_POST["nom"]) ||
+    empty($_POST["prenom"]) ||
+    empty($_POST["date_naissance"]) ||
+    empty($_POST["email"]) ||
+    empty($_POST["mdp"])
+){
+    echo "Toutes les cases doivent être remplis !";
+    header("Location: ../index.php");
+}else{
+
+    $user = new Utilisateur(array(
+        'nom' => $_POST['nom'],
+        'prenom' => $_POST['prenom'],
+        'dateNaissance' => $_POST['date_naissance'],
+        'email' => $_POST['email'],
+        'mdp' => password_hash($_POST['mdp'], PASSWORD_DEFAULT),
+
+    ));
+    var_dump($user);
+    $repository = new RepositoryUtilisateur();
+    $resultat = $repository->ajout($user);
+    var_dump($resultat);
+    if($resultat == true){
+        header("Location: ../../../vue/accueilAdmin.php");
+    }else{
+        header("Location: ../../../vue/listerUtilisateur.php");
+    }
+
+}
