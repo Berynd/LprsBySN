3926624a6783ac92a8c45db897b1f63a06a54c77
Thomas Langui <thomas.langui971@gmail.com>
2025-09-23 09:24:07 +0200

    fix(): changement de certains petit detail dans index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil - LPRS</title>
+    <title>Accueil - ProjetLprs</title>
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
@@ -16,7 +16,7 @@
                 </div>
                 <ul class="nav-menu">
                     <li class="nav-item">
-                        <a href="#accueil" class="nav-link">Accueil _aèfèeoluglizgf</a>
+                        <a href="#accueil" class="nav-link">Accueil</a>
                     </li>
                     <li class="nav-item">
                         <a href="#evenements" class="nav-link">Événements</a>
