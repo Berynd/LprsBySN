e812a7f8b6c003e5f9b0453214beb01876c9145d
Reyan BABOU <BABOU_Re@lprs.local>
2025-09-23 10:48:40 +0200

    feat: creation classe Bdd fini

--- src/bdd/Bdd.php
+++ src/bdd/Bdd.php
@@ -1,8 +1,80 @@
 <?php
+class Bdd {
+    private $nomBDD = 'projetlprs';
+    private $serveur = 'localhost';
+    private $user= 'root';
+    private $password = '';
+    private $bdd;
 
-namespace bdd;
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
 
-class Bdd
-{
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password)
+    {
+        $this->password = $password;
+    }
 
+    /**
+     * @return string
+     */
+    public function getUser(): string
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param string $user
+     */
+    public function setUser(string $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return string
+     */
+    public function getServeur(): string
+    {
+        return $this->serveur;
+    }
+
+    /**
+     * @param string $serveur
+     */
+    public function setServeur(string $serveur)
+    {
+        $this->serveur = $serveur;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNomBDD(): string
+    {
+        return $this->nomBDD;
+    }
+
+    /**
+     * @param string $nomBDD
+     */
+    public function setNomBDD(string $nomBDD)
+    {
+        $this->nomBDD = $nomBDD;
+    }
+    public function __construct()
+    {
+        $this->bdd = new PDO("mysql:dbname=".$this->nomBDD.";host=".$this->serveur, $this->user, $this->password);
+    }
+
+    public function getBdd(){
+        return $this->bdd;
+    }
 }
