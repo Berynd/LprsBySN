4be3d866f068bad52f78d641edf05d736a21932d
Berynd <gabriel.berinde93@icloud.com>
2025-09-23 10:35:40 +0200

    feat: creation des Repository (class)

--- src/modele/Formation.php
+++ src/modele/Formation.php
@@ -2,5 +2,38 @@
 
 class Formation
 {
+private $idFormation;
+private $nomFormation;
 
+    /**
+     * @return mixed
+     */
+    public function getIdFormation()
+    {
+        return $this->idFormation;
+    }
+
+    /**
+     * @param mixed $idFormation
+     */
+    public function setIdFormation($idFormation)
+    {
+        $this->idFormation = $idFormation;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNomFormation()
+    {
+        return $this->nomFormation;
+    }
+
+    /**
+     * @param mixed $nomFormation
+     */
+    public function setNomFormation($nomFormation)
+    {
+        $this->nomFormation = $nomFormation;
+    }
 }
--- src/modele/ReponseForum.php
+++ src/modele/ReponseForum.php
@@ -2,5 +2,106 @@
 
 class ReponseForum
 {
+private $idReponseForum;
+private $contenuReponseForum;
+private $dateCreationReponseForum;
+private $refPostForum;
+private $refPost;
+private $refUtilisateur;
 
+    /**
+     * @return mixed
+     */
+    public function getIdReponseForum()
+    {
+        return $this->idReponseForum;
+    }
+
+    /**
+     * @param mixed $idReponseForum
+     */
+    public function setIdReponseForum($idReponseForum)
+    {
+        $this->idReponseForum = $idReponseForum;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContenuReponseForum()
+    {
+        return $this->contenuReponseForum;
+    }
+
+    /**
+     * @param mixed $contenuReponseForum
+     */
+    public function setContenuReponseForum($contenuReponseForum)
+    {
+        $this->contenuReponseForum = $contenuReponseForum;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateCreationReponseForum()
+    {
+        return $this->dateCreationReponseForum;
+    }
+
+    /**
+     * @param mixed $dateCreationReponseForum
+     */
+    public function setDateCreationReponseForum($dateCreationReponseForum)
+    {
+        $this->dateCreationReponseForum = $dateCreationReponseForum;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRefPostForum()
+    {
+        return $this->refPostForum;
+    }
+
+    /**
+     * @param mixed $refPostForum
+     */
+    public function setRefPostForum($refPostForum)
+    {
+        $this->refPostForum = $refPostForum;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRefPost()
+    {
+        return $this->refPost;
+    }
+
+    /**
+     * @param mixed $refPost
+     */
+    public function setRefPost($refPost)
+    {
+        $this->refPost = $refPost;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRefUtilisateur()
+    {
+        return $this->refUtilisateur;
+    }
+
+    /**
+     * @param mixed $refUtilisateur
+     */
+    public function setRefUtilisateur($refUtilisateur)
+    {
+        $this->refUtilisateur = $refUtilisateur;
+    }
 }
