ec823fb00f4999dec9dd905246e06d25b1f69869
Reyan BABOU <BABOU_Re@lprs.local>
2025-09-23 10:10:45 +0200

    modele entreprise

--- src/modele/Entreprise.php
+++ src/modele/Entreprise.php
@@ -2,5 +2,76 @@
 
 class Entreprise
 {
+private $idEntreprise;
+private $nom;
+
+private $adresse;
+
+private $site_web;
+
+    /**
+     * @return mixed
+     */
+    public function getIdEntreprise()
+    {
+        return $this->idEntreprise;
+    }
+
+    /**
+     * @param mixed $idEntreprise
+     */
+    public function setIdEntreprise($idEntreprise)
+    {
+        $this->idEntreprise = $idEntreprise;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * @param mixed $adresse
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSiteWeb()
+    {
+        return $this->site_web;
+    }
+
+    /**
+     * @param mixed $site_web
+     */
+    public function setSiteWeb($site_web)
+    {
+        $this->site_web = $site_web;
+    }
+    
 
 }
