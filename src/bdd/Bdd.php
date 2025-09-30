5a1d63c149db627d60a85f3e690266595eb5aa2c
Eythanne <e.passard@lprs.frgit config --global user.email e.passard@lprs.fr>
2025-09-30 08:36:18 +0200

    modification bdd et repo

--- src/bdd/Bdd.php
+++ src/bdd/Bdd.php
@@ -1,22 +1,12 @@
 <?php
 
-namespace bdd;
-use PDO;
-use PDOException;
-
-class Bdd
+class BDD
 {
     private $bdd;
 
     public function __construct()
     {
-        try {
             $this->bdd = new PDO('mysql:host=localhost;dbname=projetlprs;charset=utf8', 'root', '');
-            echo 'reussi';
-
-        }catch (PDOException $e){
-            echo 'Erreur de connexion a la bdd : '.$e->getMessage();
-        }
     }
 
     public function getBdd()
--- src/bdd/Test.php
+++
@@ -1,3 +0,0 @@
-<?php
-include 'Bdd.php';
-new \bdd\Bdd();
