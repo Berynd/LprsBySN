c51a78d9ff900745292768036980a27d05d5d440
Berynd <gabriel.berinde93@icloud.com>
2025-10-27 11:45:40 +0100

    merge:pull

--- src/traitement/ModificationTraitement.php
+++ src/traitement/ModificationTraitement.php
@@ -1,4 +1,20 @@
 <?php
 include "../repository/UtilisateurRepository.php";
 require_once "../bdd/BDD.php";
-require_once "../utilisateur/Utilisateur.php";
+require_once "../modele/Utilisateur.php";
+session_start();
+
+
+
+
+$user = new Utilisateur(array(
+    'nom' => $_POST['nom'],
+    'prenom' => $_POST['prenom'],
+    'email' => $_POST['email'],
+    'idUtilisateur' => $_POST['idUtilisateur']
+));
+
+var_dump($user);
+$repository = new UtilisateurRepository();
+$resultat = $repository->modificationAdmin($user);
+header("Location:../../vue/page_admin.php");
