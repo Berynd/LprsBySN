f5ea7d015c3f4ed89f8bf8722ae58be872bca85d
Thomas Langui <thomas.langui971@gmail.com>
2025-12-01 09:38:27 +0100

    feat(): ajout de la réglementation RGPD.

--- src/traitement/TraitementInscriptionUtilisateur.php
+++ src/traitement/TraitementInscriptionUtilisateur.php
@@ -1,7 +1,31 @@
 <?php
+function validerMotDePasse(string $mdp): array {
+    $erreurs = [];
+
+    if (strlen($mdp) < 12) {
+        $erreurs[] = "Le mot de passe doit contenir au moins 12 caractères.";
+    }
+    if (!preg_match('/[A-Z]/', $mdp)) {
+        $erreurs[] = "Le mot de passe doit contenir au moins une majuscule.";
+    }
+    if (!preg_match('/[a-z]/', $mdp)) {
+        $erreurs[] = "Le mot de passe doit contenir au moins une minuscule.";
+    }
+    if (!preg_match('/\d/', $mdp)) {
+        $erreurs[] = "Le mot de passe doit contenir au moins un chiffre.";
+    }
+    if (!preg_match('/[\W_]/', $mdp)) {
+        $erreurs[] = "Le mot de passe doit contenir au moins un caractère spécial.";
+    }
+
+    return $erreurs;
+}
+
+
 include "../repository/UtilisateurRepository.php";
 require_once "../bdd/BDD.php";
 require_once "../modele/Utilisateur.php";
+
 var_dump($_POST);
 
 if (empty($_POST["nom"]) ||
@@ -9,26 +33,37 @@ if (empty($_POST["nom"]) ||
     empty($_POST["email"]) ||
     empty($_POST["mdp"])
 ) {
-    echo "C'est pas bien tetard";
+    echo "Veuillez remplir tous les champs.";
     header("Location: ../../vue/Connexion.php");
-} else {
+    exit();
+}
 
-    $user = new Utilisateur(array(
-        'nom' => $_POST['nom'],
-        'prenom' => $_POST['prenom'],
-        'email' => $_POST['email'],
-        'mdp' => password_hash($_POST['mdp'], PASSWORD_DEFAULT),
 
-    ));
+$erreurs = validerMotDePasse($_POST['mdp']);
 
-    var_dump($user);
-    $repository = new UtilisateurRepository();
-    $resultat = $repository->inscription($user);
-    var_dump($resultat);
-    if ($resultat == true) {
-        header("Location: ../../vue/Connexion.php");
-    } else {
-        header("Location: ../../index.php");
+if (!empty($erreurs)) {
+
+    foreach ($erreurs as $err) {
+        echo $err . "<br>";
     }
 
+
+    header("Location: ../../vue/Inscription.php");
+    exit();
+}
+
+$user = new Utilisateur(array(
+    'nom' => $_POST['nom'],
+    'prenom' => $_POST['prenom'],
+    'email' => $_POST['email'],
+    'mdp' => password_hash($_POST['mdp'], PASSWORD_DEFAULT),
+));
+
+$repository = new UtilisateurRepository();
+$resultat = $repository->inscription($user);
+
+if ($resultat == true) {
+    header("Location: ../../vue/Connexion.php");
+} else {
+    header("Location: ../../index.php");
 }
--- src/traitement/Utilisateur/TraitementAjoutUtilisateur.php
+++ src/traitement/Utilisateur/TraitementAjoutUtilisateur.php
@@ -1,35 +1,69 @@
 <?php
+function validerMotDePasseAdmin(string $mdp): array {
+    $erreurs = [];
+
+    if (strlen($mdp) < 12) {
+        $erreurs[] = "Le mot de passe doit contenir au moins 12 caractères.";
+    }
+    if (!preg_match('/[A-Z]/', $mdp)) {
+        $erreurs[] = "Le mot de passe doit contenir au moins une majuscule.";
+    }
+    if (!preg_match('/[a-z]/', $mdp)) {
+        $erreurs[] = "Le mot de passe doit contenir au moins une minuscule.";
+    }
+    if (!preg_match('/\d/', $mdp)) {
+        $erreurs[] = "Le mot de passe doit contenir au moins un chiffre.";
+    }
+    if (!preg_match('/[\W_]/', $mdp)) {
+        $erreurs[] = "Le mot de passe doit contenir au moins un caractère spécial.";
+    }
+
+    return $erreurs;
+}
+
 
 include "../../repository/UtilisateurRepository.php";
-require_once "../../bdd/Bdd.php";
+require_once "../../bdd/BDD.php";
 require_once "../../modele/Utilisateur.php";
-require_once "../../repository/UtilisateurRepository.php";
+
 var_dump($_POST);
 
-if(empty($_POST["nom"]) ||
+if (empty($_POST["nom"]) ||
     empty($_POST["prenom"]) ||
     empty($_POST["email"]) ||
     empty($_POST["mdp"])
-){
-    echo "Toutes les cases doivent être remplis !";
-    header("Location: ../../connexionUtilisateur.php");
-}else{
+) {
+    echo "Veuillez remplir tous les champs.";
+    header("Location: ../../vue/Connexion.php");
+    exit();
+}
 
-    $user = new Utilisateur(array(
-        'nom' => $_POST['nom'],
-        'prenom' => $_POST['prenom'],
-        'email' => $_POST['email'],
-        'mdp' => password_hash($_POST['mdp'], PASSWORD_DEFAULT),
 
-    ));
-    var_dump($user);
-    $repository = new UtilisateurRepository();
-    $resultat = $repository->ajout($user);
-    var_dump($resultat);
-    if($resultat == true){
-        header("Location: ../../../vue/PageAdmin.php");
-    }else{
-        header("Location: ../../../vue/PageAdmin.php?page=utilisateur");
+$erreurs = validerMotDePasseAdmin($_POST['mdp']);
+
+if (!empty($erreurs)) {
+
+    foreach ($erreurs as $err) {
+        echo $err . "<br>";
     }
 
+
+    header("Location: ../../vue/PageAdmin.php?page=utilisateur");
+    exit();
+}
+
+$user = new Utilisateur(array(
+    'nom' => $_POST['nom'],
+    'prenom' => $_POST['prenom'],
+    'email' => $_POST['email'],
+    'mdp' => password_hash($_POST['mdp'], PASSWORD_DEFAULT),
+));
+
+$repository = new UtilisateurRepository();
+$resultat = $repository->ajout($user);
+
+if ($resultat == true) {
+    header("Location: ../../../vue/PageAdmin.php");
+} else {
+    header("Location: ../../../vue/PageAdmin.php?page=utilisateur");
 }
